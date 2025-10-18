63eceb684fc0c8e9a9470e88d06820f7241685c0
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2025-10-18 21:42:11 +0200

    Modification of the project s structure

---
+++ src/lib.h
@@ -0,0 +1,20 @@
+
+/*Led verdi spenti
+  Led rosso lampeggiante
+  LCD scritta "Welcome to TOS/n press b1 to start".
+  Se non viene premuto niente in 10 sec, il sistema entra in deep sleep;
+  altrimenti, se viene premuto b1, il gioco inizia*/
+void begin();
+
+void sleep();
+
+/* Tutti i led spenti
+   LCD compare la scritta: "Go!"
+   punteggio = 0*/
+void startRound();
+
+void correctSequence();
+
+void wrongSequence();
+
+void stop();
